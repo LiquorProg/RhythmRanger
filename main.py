fd95ada096791974bc0b01039bf9bbde5441f0d6
sasha <sashaskvorsov11@gmail.com>
2023-05-09 21:06:16 +0300

    Добавил команду "!help". Поменял команду "!queue", теперь вместо ссылок команда выводит названия песен. Поменял функцию play_song, теперь при включении нового трека, бот высылает сообщение в чат о том какой трек сейчас играет.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import discord
 import asyncio
 from pytube import YouTube
@@ -9,6 +11,8 @@ from config import config
 intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix=config["prefix"], intents=intents)
+
+bot.remove_command('help')
 
 # Создаем пустую очередь треков
 queue_of_music = deque()
@@ -65,6 +69,9 @@ async def play_song(ctx):
     # Проигрываем текущий трек
     voice_client.play(discord.FFmpegPCMAudio(audio_url, executable="ffmpeg\\bin\\ffmpeg.exe"), after=lambda e: asyncio.run_coroutine_threadsafe(play_song(ctx), bot.loop))
 
+    # Отправляем сообщение о начале проигрывания нового трека
+    await ctx.send(f"Сейчас играет: {video.title}")
+
 @bot.command()
 async def skip(ctx):
     voice_client = ctx.voice_client
@@ -117,8 +124,22 @@ async def queue(ctx):
     if len(queue_of_music) == 0:
         await ctx.send("Очередь пуста.")
     else:
-        queue_list = "\n".join(queue_of_music)
+        queue_list = "\n".join([f"{index + 1}. {YouTube(url).title}" for index, url in enumerate(queue_of_music)])
         await ctx.send(f"Очередь песен:\n{queue_list}")
+
+@bot.command()
+async def help(ctx):
+    emb = discord.Embed(title="Информация о командах", color=random.randint(1, 16777216))
+    emb.add_field(name=f"""`{config["prefix"]}help` : """, value="Вызовет это меню", inline=True)
+    emb.add_field(name=f"""`{config["prefix"]}join` : """, value="присоединить бота к голосовому каналу", inline=True)
+    emb.add_field(name=f"""`{config["prefix"]}leave` : """, value="отключить бота от голосового канала", inline=True)
+    emb.add_field(name=f"""`{config["prefix"]}play` : """, value="воспроизвести песню по указанному URL", inline=True)
+    emb.add_field(name=f"""`{config["prefix"]}skip` : """, value="пропустить текущую песню", inline=True)
+    emb.add_field(name=f"""`{config["prefix"]}stop` : """, value="остановить проигрывание и очистить очередь", inline=True)
+    emb.add_field(name=f"""`{config["prefix"]}pause` : """, value="приостановить проигрывание", inline=True)
+    emb.add_field(name=f"""`{config["prefix"]}resume` : """, value="возобновить проигрывание", inline=True)
+    emb.add_field(name=f"""`{config["prefix"]}queue` : """, value="показать текущую очередь песен", inline=True)
+    await ctx.send(embed=emb)
 
 if __name__ == "__main__":
     bot.run(config["token"])
